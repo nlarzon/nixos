64cf4f9356a1b29a8b07b920eb836159539c53a3
Niklas Larsson <nlarzon@gmail.com>
2024-03-03 12:16:11 +0100

    Enable experimental-features
    Enable use of experimental-feature flakes

--- configuration.nix
+++ configuration.nix
@@ -10,6 +10,10 @@
       ./hardware-configuration.nix
     ];
 
+
+  # Enable "experimental" features
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # Bootloader.
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda";
