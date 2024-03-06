80af25b68b4a687a5484408433d95fa51aa41200
Niklas Larsson <nlarzon@gmail.com>
2024-03-06 08:42:49 +0100

    Add wallpaper managed by home-manager
    Copies the wallpaper image to the correct place

--- modules/home-manager/home.nix
+++ modules/home-manager/home.nix
@@ -4,8 +4,8 @@
   home.homeDirectory = "/home/nlarzon";
   home.stateVersion = "23.11";
 
-  home.file."/home/theo/Documents/wallpapers/solidcolor.png" = {
-    enable = false; # disable this if you want to add your own wallpaper
-    source = ./wallpaper.png; # or change this
+  home.file."/home/nlarzon/Documents/wallpapers/ethereal-tech-3440x1440.webp" = {
+    enable = true; # disable this if you want to add your own wallpaper
+    source = ./ethereal-tech-3440x1440.webp; # or change this
   };
 }
