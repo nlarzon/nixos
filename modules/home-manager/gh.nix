ebcfe4046ce36207b247af8ab862741b68c0c4cc
Niklas Larsson <nlarzon@gmail.com>
2024-03-06 08:27:26 +0100

    Add gh to home-manager
    This adds gh cli with my configs

---
+++ modules/home-manager/gh.nix
@@ -0,0 +1,15 @@
+{inputs, outputs, pkgs, config, ...}:
+{
+  programs.gh = {
+    enable = true;
+    extensions = [
+      pkgs.gh-dash
+    ];
+   settings = {
+    aliases = {
+      co = "pr checkout";
+      pl = "pr list";
+      ga = "dash";
+    };
+  };
+}
