dca154f8bfd03be0e8ce6286c94a676bb443acc7
nlarzon <nlarzon@gmail.com>
2024-04-06 10:50:25 +0200

    Fix polybar start

--- modules/home-manager/bspwm.nix
+++ modules/home-manager/bspwm.nix
@@ -4,7 +4,7 @@
 #! /usr/bin/env bash
 
 pgrep -x sxhkd >/dev/null || sxhkd &
-wal -R -q &
+# wal -R -q &
 
 # Set the cursor
 xsetroot -cursor_name left_ptr &
@@ -12,7 +12,7 @@ xsetroot -cursor_name left_ptr &
 # pgrep -x picom >/dev/null || picom --config ~/.config/picom/picom.conf &
 pgrep -x polybar > /dev/null || "$HOME"/.config/polybar/launch.sh &
 # pkill copyq || copyq -s main &
-deamonize ~/.config/polybar/launch.sh
+#deamonize ~/.config/polybar/launch.sh
 
 
 bspc config remove_disabled_monitors true
--- modules/home-manager/xinit.nix
+++ modules/home-manager/xinit.nix
@@ -2,7 +2,9 @@
 {
   xdg.configFile."/home/nlarzon/.xprofile".text = ''
 #  exec bspwm &
-#  exec /home/nlarzon/.config/polybar/launch.sh &
+#
+exec wal -R -q &
+exec /home/nlarzon/.config/polybar/launch.sh &
 #  exec picom &
    exec sxhkd &
 #  exec xset r rate 180 70 &
